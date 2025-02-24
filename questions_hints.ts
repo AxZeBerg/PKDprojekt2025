0059cfb2e28c76b8783e5ff11002432bd494d4c0
Alberthedstrom <albert.hedstrom.2739@student.uu.se>
2025-02-24 15:36:13 +0100

    Frågor och hints

---
+++ questions_hints.ts
@@ -0,0 +1,79 @@
+export const Hints10_Sweden = ["This country had around 593 billion USD BNP last year", "A northern mining town in this country is being relocated",
+                      "This country became a member of the EU in 1995", "This country have donated approximately 6,45 billion Euros worth of supplies to Ukraine",
+                      "This country has over 200.000 islands"];
+export const Hints8_Sweden = ["Women could vote in this country 1919", "This country has a unique law that grants people the right to roam freely across private and public land",
+                     "In parts of this country, the sun never sets", "This country has won Eurovision 7 times",
+                     "Many houses in this country is painted in a deep red color"];
+export const Hints6_Sweden = ["The largest lake in this country is called Vänern", "This country was neutral in both World Wars",
+                     "This country have a hotel entirely made of ice", "A russian war-submarine once ran aground by the coast of this country",
+                     "A prime minister of this country was murdered 1986"];
+export const Hints4_Sweden = ["Zlatan Ibrahimovic was born in this country", "Astrid Lindgren was born in this country", 
+                     "This countries flag is blue and yellow", "Volvo was created in this country",
+                     "In parts of this country the sun never sets"];
+export const Hints2_Sweden = ["Stockholm is the capital of this country", "This countries neighbors are Norway, Finland and Denmark", 
+                     "IKEA was created in this country", "The Prime Minister of this country is Ulf Kristersson",
+                     "The amazing tv-show På Spåret runs on fridays in this country"];
+
+export const Questions_Sweden = [pair("What city in Sweden is home to the best University in the world?", "uppsala"),
+                          pair("How many kings did Sweden have during the 1900's", "4"),
+                          pair("How many cities with over 200.000 inhabitants does Sweden have?", "3"),
+                          pair("What city in Sweden is Veronica Maggio from", "uppsala"),
+                          pair("What city is the great Albert Hedström from?", "västervik"),
+                          pair("What was the pop-group from sweden called that ruled the music world during the 70's", "abba")
+];
+
+export const Hints10_Germany = ["This country had around 4.3 trillion USD GDP last year", "A northern city in this country is home to one of the world's largest ports",  
+                         "This country became a member of the EU in 1958", "This country has donated approximately 17 billion Euros worth of supplies to Ukraine",  
+                         "This country shares borders with 9 other countries"];
+
+export const Hints8_Germany = ["Women could vote in this country starting in 1918", "This country has a famous highway with no speed limit on some parts",  
+                        "In parts of this country, Oktoberfest is a huge tradition", "This country has won Eurovision twice",  
+                        "Many houses in this country have timber-framed architecture"];
+
+export const Hints6_Germany = ["The largest lake in this country is called Lake Constance (Bodensee)", "This country was divided into two separate states for over 40 years",  
+                        "This country has a castle that inspired Disney's Cinderella Castle", "A wall once split the capital of this country into two parts",  
+                        "A chancellor of this country was involved in reunifying it in 1990"];
+
+export const Hints4_Germany = ["Thomas Müller was born in this country", "The famous composer Ludwig van Beethoven was born in this country",  
+                        "This country's flag is black, red, and gold", "Volkswagen was created in this country",  
+                        "This country is famous for its beer and sausages"];
+
+export const Hints2_Germany = ["Berlin is the capital of this country", "This country's neighbors include France, Poland, and Austria",  
+                        "BMW was created in this country", "The Chancellor of this country is Olaf Scholz",  
+                        "The Bundesliga is the top football league in this country"];
+
+export const Questions_Germany = [pair("What city in Germany is home to the best University in the country?", "heidelberg"),  
+                           pair("How many chancellors did Germany have during the 1900s?", "7"),  
+                           pair("How many cities with over 1 million inhabitants does Germany have?", "4"),  
+                           pair("What city in Germany is Rammstein from?", "berlin"),  
+                           pair("What city is the great Johann Wolfgang von Goethe from?", "frankfurt"),  
+                           pair("What was the German electronic music band called that revolutionized the genre?", "kraftwerk")  
+];
+
+export const Hints10_NewZealand = ["This country had around 250 billion USD GDP last year", "A major city in this country is built on an active volcanic field",  
+                            "This country became a member of the United Nations in 1945", "This country has donated approximately 80 million NZD worth of supplies to Ukraine",  
+                            "This country consists of two main islands and over 600 smaller islands"];
+
+export const Hints8_NewZealand = ["Women could vote in this country starting in 1893", "This country has no native land mammals except for bats",  
+                           "In parts of this country, geothermal activity creates natural hot springs", "This country has won Eurovision 0 times because it doesn’t compete",  
+                           "Many houses in this country have earthquake-resistant architecture"];
+
+export const Hints6_NewZealand = ["The largest lake in this country is called Lake Taupō", "This country was the first to introduce the 8-hour workday",  
+                           "This country has a town with the longest place name in the world", "A famous set of movies was filmed in this country",  
+                           "A major city in this country was hit by a devastating earthquake in 2011"];
+
+export const Hints4_NewZealand = ["Steven Adams was born in this country", "The famous director Peter Jackson was born in this country",  
+                           "This country’s flag includes the Union Jack and the Southern Cross", "Air New Zealand is the national airline of this country",  
+                           "The Māori culture is an important part of this country’s identity"];
+
+export const Hints2_NewZealand = ["Wellington is the capital of this country", "This country's neighbors include Australia and several Pacific Island nations",  
+                           "The All Blacks are the national rugby team of this country", "The Prime Minister of this country is Christopher Luxon",  
+                           "The kiwi is a national symbol of this country"];
+
+export const Questions_NewZealand = [pair("What city in New Zealand is home to the best university in the country?", "auckland"),  
+                              pair("How many prime ministers did New Zealand have during the 1900s?", "20"),  
+                              pair("How many cities with over 100,000 inhabitants does New Zealand have?", "6"),  
+                              pair("What city in New Zealand is Lorde from?", "auckland"),  
+                              pair("What city is the great Ernest Rutherford from?", "nelson"),  
+                              pair("What is the name of the trilogy that was filmed in New Zealand and made the country a tourism hotspot?", "lord of the rings")  
+];
